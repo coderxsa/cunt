40bb34cfaacf25d632d5ceb784a5dbf8a01a544e
coderxsa <coderxfame@gmail.com>
2025-04-19 15:35:10 +0200

    CUNT

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'CODERXSA',
   botName: "مهبل",
   botOwner: 'CODERXSA', // Your name
-  ownerNumber: '', //Your number
+  ownerNumber: '27717311486', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
